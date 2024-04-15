425eac26eaf5a88f5aaa87c15982cd72528c61c0
fmpberger88 <fabian.berger@protonmail.ch>
2024-04-15 23:36:57 +0200

    adding testframework for todoModule

--- modules/todoModule.js
+++ modules/todoModule.js
@@ -4,7 +4,7 @@ const todoModules = (() => {
     let todoList = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
 
     const saveToLocalStorage = () => {
-        localstorage.setItem(STORAGE_KEY, JSON.stringify(todoList));
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(todoList));
     };
 
     const addTodo = (title, description, dueDate, priority, notes, checklist) => {
@@ -17,7 +17,7 @@ const todoModules = (() => {
             notes,
             checklist,
             completed: false
-        }
+        };
         todoList.push(todo);
         saveToLocalStorage();
     };
@@ -26,12 +26,11 @@ const todoModules = (() => {
 
     const completeTodo = (id) => {
         const index = todoList.findIndex(todo => todo.id === id);
-
         if (index !== -1) {
             todoList[index].completed = true;
             saveToLocalStorage();
         }
-    }
+    };
 
     const deleteTodo = (id) => {
         const index = todoList.findIndex(todo => todo.id === id);
@@ -39,12 +38,14 @@ const todoModules = (() => {
             todoList.splice(index, 1);
             saveToLocalStorage();
         }
-    }
+    };
 
     return {
         addTodo,
         getTodos,
         completeTodo,
         deleteTodo,
-    }
-})
+    };
+})();
+
+export default todoModules;
