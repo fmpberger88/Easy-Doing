82475e78b664e4f02be9686d98f6189b10700315
fmpberger88 <fabian.berger@protonmail.ch>
2024-04-16 18:12:17 +0200

    adding parameter document

--- utils/showProjects.js
+++ utils/showProjects.js
@@ -1,11 +1,13 @@
 import projectModule from "../modules/projectModule.js";
 
-const showProjects = () => {
+const showProjects = (document) => {
     // get all projects
     const projects = projectModule.getProjects();
     projects.forEach(project => {
         const projectElement = document.createElement("li");
         projectElement.textContent = project.title;
         document.querySelector('#project-list').appendChild(projectElement);
-    })
+    });
 }
+
+export default showProjects;
