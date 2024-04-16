8deb68465a7bbc10b5a775abe1d2efe438cef784
fmpberger88 <fabian.berger@protonmail.ch>
2024-04-16 18:12:44 +0200

    adding parameter document to showProjects

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import {seedProjects} from "./seed.js";
 import projectModule from "./modules/projectModule.js";
 import todoModule from "./modules/todoModule.js";
+import showProjects from "./utils/showProjects.js";
 
-if (!projectModule.getProjects().length || todoModule.getTodos().length) {
+if (!projectModule.getProjects().length || !todoModule.getTodos().length) {
     seedProjects();
 }
+
+showProjects(document);
